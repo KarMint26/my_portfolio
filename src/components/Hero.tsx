1007f65a5f6bee78bc1286480f812c007e79848f
KarMint26 <kareltrisnanto26@gmail.com>
2025-11-19 11:08:22 +0700

    feat(hero&stats): add hero and stats section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import { motion } from 'motion/react';
+import { fadeUp, staggerContainer } from '@/lib/animation';
+import { Button } from './ui/button';
+import { SparkleIcon } from 'lucide-react';
 
 export default function Hero() {
   return (
-    <React.Fragment>
-      <h1>Hero</h1>
-    </React.Fragment>
+    <motion.section
+      initial='hidden'
+      whileInView='visible'
+      viewport={{ once: true, amount: 0.3 }}
+      variants={staggerContainer(0)}
+      className='pt-20'
+      id='hero'
+    >
+      <motion.p
+        variants={fadeUp}
+        className='flex items-center justify-center py-1 gap-2 border border-neutral-600 rounded-sm w-32'
+      >
+        <SparkleIcon size={15} /> <span>Introduction</span>
+      </motion.p>
+
+      <motion.h1
+        variants={fadeUp}
+        className='text-4xl md:text-5xl font-semibold capitalize mt-2 max-w-3xl md:leading-16'
+      >
+        I'm <span className='text-primary'>Karel</span> Web Developer & Frontend
+        Developer.
+      </motion.h1>
+
+      <motion.div
+        variants={fadeUp}
+        className='mt-5 flex gap-2'
+      >
+        <Button asChild>
+          <a href='#projects'>My projects</a>
+        </Button>
+
+        <Button variant='outline'>Download CV</Button>
+      </motion.div>
+    </motion.section>
   );
 }
